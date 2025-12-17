b95d4547bf3a295974bcef204b4ed247a1afd788
Tyler Jaacks <TylerJaacks@Gmail.com>
2025-12-17 17:09:00 -0600

    Updated codebase on https://github.com/terryky/android_openxr_gles.

--- app/cpp/main.cpp
+++ app/cpp/main.cpp
@@ -1,35 +1,86 @@
-#include "HelloQuest.h"
+#include "app_engine.h"
 
-android_app* HelloQuest::androidApp = nullptr;
-HelloQuest::AndroidAppState HelloQuest::androidAppState{};
+struct AndroidAppState {
+	ANativeWindow* NativeWindow = nullptr;
+	bool Resumed = false;
+};
 
-void android_main(struct android_app* app) {
-	JNIEnv* env = nullptr;
-	app->activity->vm->AttachCurrentThread(&env, nullptr);
+static void
+ProcessAndroidCmd (struct android_app* app, int32_t cmd)
+{
+	AndroidAppState* appState = (AndroidAppState*)app->userData;
 
-	PFN_xrInitializeLoaderKHR xrInitializeLoaderKHR = nullptr;
-	OPENXR_CHECK(
-			xrGetInstanceProcAddr(XR_NULL_HANDLE, "xrInitializeLoaderKHR",
-								  reinterpret_cast<PFN_xrVoidFunction*>(&xrInitializeLoaderKHR)),
-			"Failed to get xrInitializeLoaderKHR");
+	switch (cmd) {
+		case APP_CMD_START:
+			LOGI ("APP_CMD_START");
+			break;
 
-	if (!xrInitializeLoaderKHR)
-		return;
+		case APP_CMD_RESUME:
+			LOGI ("APP_CMD_RESUME");
+			appState->Resumed = true;
+			break;
 
-	XrLoaderInitInfoAndroidKHR loaderInitInfo{
-			XR_TYPE_LOADER_INIT_INFO_ANDROID_KHR};
-	loaderInitInfo.applicationVM = app->activity->vm;
-	loaderInitInfo.applicationContext = app->activity->clazz;
+		case APP_CMD_PAUSE:
+			LOGI ("APP_CMD_PAUSE");
+			appState->Resumed = false;
+			break;
 
-	OPENXR_CHECK(xrInitializeLoaderKHR(
-			reinterpret_cast<XrLoaderInitInfoBaseHeaderKHR*>(&loaderInitInfo)),
-			"Failed to initialize OpenXR loader");
+		case APP_CMD_STOP:
+			LOGI ("APP_CMD_STOP");
+			break;
 
-	app->userData = &HelloQuest::androidAppState;
-	app->onAppCmd = HelloQuest::AndroidAppHandleCmd;
+		case APP_CMD_DESTROY:
+			LOGI ("APP_CMD_DESTROY");
+			appState->NativeWindow = NULL;
+			break;
 
-	HelloQuest::androidApp = app;
+			// The window is being shown, get it ready.
+		case APP_CMD_INIT_WINDOW:
+			LOGI ("APP_CMD_INIT_WINDOW");
+			appState->NativeWindow = app->window;
+			break;
 
-	HelloQuest helloQuest;
-	helloQuest.Run();
+			// The window is being hidden or closed, clean it up.
+		case APP_CMD_TERM_WINDOW:
+			LOGI ("APP_CMD_TERM_WINDOW");
+			appState->NativeWindow = NULL;
+			break;
+	}
+}
+
+
+/*--------------------------------------------------------------------------- *
+ *      M A I N    F U N C T I O N
+ *--------------------------------------------------------------------------- */
+void android_main(struct android_app* app)
+{
+	AndroidAppState appState = {};
+	app->userData = &appState;
+	app->onAppCmd = ProcessAndroidCmd;
+
+	AppEngine engine (app);
+	engine.InitOpenXR_GLES();
+
+	while (app->destroyRequested == 0)
+	{
+		// Read all pending events.
+		for (;;) {
+			int events;
+			struct android_poll_source* source;
+
+			int timeout = -1; // blocking
+			if (appState.Resumed || oxr_is_session_running() || app->destroyRequested)
+				timeout = 0;  // non blocking
+
+			if (ALooper_pollAll(timeout, nullptr, &events, (void**)&source) < 0) {
+				break;
+			}
+
+			if (source != nullptr) {
+				source->process(app, source);
+			}
+		}
+
+		engine.UpdateFrame();
+	}
 }
